851f00e716a97b57afb4cfe545c42240aa49ac12
morven <mgqfh1221@163.com>
2019-06-05 12:05:06 +0800

    update no2

--- src/main/java/src/leetcode/no2/Solution1.java
+++ src/main/java/src/leetcode/no2/Solution1.java
@@ -1,52 +1,57 @@
 package src.leetcode.no2;
 
-import java.util.List;
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * <p>
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Example:
+ * <p>
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
 
 public class Solution1 {
 
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        long i = 0;
-        long num1 = 0;
-        long num2 = 0;
+        int left = 0;
         ListNode next1 = l1;
         ListNode next2 = l2;
+        ListNode result = new ListNode(0);
+        ListNode last = result;
         while (next1 != null || next2 != null) {
+            int temp1 = 0;
+            int temp2 = 0;
             if (next1 != null) {
-                num1 = next1.val * (long) Math.pow(10, i) + num1;
+                temp1 = next1.val;
                 next1 = next1.next;
             }
             if (next2 != null) {
-                num2 = next2.val * (long) Math.pow(10, i) + num2;
-
+                temp2 = next2.val;
                 next2 = next2.next;
             }
-            i++;
+            int sum = temp1 + temp2 + left;
+            left = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            last.next = node;
+            last = node;
         }
-        System.out.println(num1);
-        System.out.println(num2 + num1);
-        char[] result = String.valueOf(num1 + num2).toCharArray();
-        ListNode node = new ListNode(Integer.parseInt(result[result.length - 1] + ""));
-        ListNode last = node;
-        for (int j = result.length - 2; j >= 0; j--) {
-            String temp = String.valueOf(result[j]);
-            ListNode node1 = new ListNode(Integer.parseInt(temp));
-            System.out.println(node1.val);
-            last.next = node1;
-            last = node1;
+        if (left > 0) {
+            last.next = new ListNode(1);
         }
-        return node;
+        return result.next;
     }
 
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(9);
+        ListNode l1 = new ListNode(5);
         ListNode l2 = new ListNode(9);
         l1.next = l2;
 
 
-        ListNode l4 = new ListNode(9);
+        ListNode l4 = new ListNode(5);
 
-        long a  = 9223372036854775808l;
         Solution1 solution1 = new Solution1();
 
         System.out.println(solution1.addTwoNumbers(l1, l4).toString());
